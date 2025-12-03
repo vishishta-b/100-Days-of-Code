81f4a2bc46c638c2ab4b6dbcde6dbbd254f64866
Vishishta Biswas <77668338+vishishta-b@users.noreply.github.com>
2025-12-03 16:11:19 +0530

    Update NumSwap.c

--- Q1 - Q10/Day-4/NumSwap.c
+++ Q1 - Q10/Day-4/NumSwap.c
@@ -1,6 +1,6 @@
 //Write a program to swap two numbers using a third variable.
-//Name: Vatsal Agarwal
-//SAP ID: 590023223 
+//Name: Vishishta Biswas
+//SAP ID: 590025579
 
 #include <stdio.h>
 int main() 
