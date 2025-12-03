a5d084b47c9519c354e7006afda9ddee514e8449
Vishishta Biswas <77668338+vishishta-b@users.noreply.github.com>
2025-12-03 16:09:30 +0530

    Update author name and SAP ID in Rectangle.c

--- Q1 - Q10/Day-2/Rectangle.c
+++ Q1 - Q10/Day-2/Rectangle.c
@@ -1,6 +1,6 @@
 //Write a program to calculate the area and perimeter of a rectangle given its length and breadth.
-//Name: Vatsal Agarwal
-//SAP ID: 590023223 
+//Name: Vishistha Biswas
+//SAP ID: 590025579
 
 #include <stdio.h>
 int main() 
